21d02b38af13dbab3f108b1eede266b8c4714dca
Matteo Velludini <matteo.velludini@geo-solutions.it>
2017-03-21 15:40:07 +0100

    fixed build

--- prod-webpack.config.js
+++ prod-webpack.config.js
@@ -4,10 +4,20 @@ var LoaderOptionsPlugin = require("webpack/lib/LoaderOptionsPlugin");
 var ParallelUglifyPlugin = require("webpack-parallel-uglify-plugin");
 var DefinePlugin = require("webpack/lib/DefinePlugin");
 var NormalModuleReplacementPlugin = require("webpack/lib/NormalModuleReplacementPlugin");
+var NoEmitOnErrorsPlugin = require("webpack/lib/NoEmitOnErrorsPlugin");
+const extractThemesPlugin = require('./MapStore2/themes.js').extractThemesPlugin;
 
 webpackConfig.plugins = [
     new LoaderOptionsPlugin({
-        debug: false
+        debug: false,
+        options: {
+            postcss: {
+                plugins: [
+                    require('postcss-prefix-selector')({prefix: '.ms2', exclude: ['.ms2']})
+                ]
+            },
+            context: __dirname
+        }
     }),
     new DefinePlugin({
         "__DEVTOOLS__": false
@@ -27,7 +37,9 @@ webpackConfig.plugins = [
             compress: {warnings: false},
             mangle: true
         }
-    })
+    }),
+    new NoEmitOnErrorsPlugin(),
+    extractThemesPlugin
 ];
 webpackConfig.devtool = undefined;
 
