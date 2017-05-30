d84b1ea15623d9f5013bf77371b38afbfaefa299
stefano bovio <stefano.bovio@geo-solutions.it>
2017-05-30 17:57:55 +0200

    Updated MapStore2 Submodule with the Release 2017.03.01  (#8)
    * Fixed configuration to support new plugins
    
    * Updated available plugins
    
    * Updated MapStore2 submodule

--- js/app.jsx
+++ js/app.jsx
@@ -5,34 +5,63 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
 const React = require('react');
 const ReactDOM = require('react-dom');
 const {connect} = require('react-redux');
-
-const StandardApp = require('../MapStore2/web/client/components/app/StandardApp');
+const LocaleUtils = require('../MapStore2/web/client/utils/LocaleUtils');
 const {registerSearchServiceEpic} = require('./epics/search');
-const {pages, pluginsDef, initialState, storeOpts, printingEnabled} = require('./appConfig');
 
-const StandardRouter = connect((state) => ({
-    locale: state.locale || {},
-    pages
-}))(require('../MapStore2/web/client/components/app/StandardRouter'));
+const startApp = () => {
+    const ConfigUtils = require('../MapStore2/web/client/utils/ConfigUtils');
 
-const initialActions = [];
-const appStore = require('../MapStore2/web/client/stores/StandardStore').bind(null, initialState, {
-    maps: require('../MapStore2/web/client/reducers/maps'),
-    security: require('../MapStore2/web/client/reducers/security')
-}, {registerSearchServiceEpic});
-const appConfig = {
-    appStore,
-    storeOpts,
-    pluginsDef,
-    initialActions,
-    appComponent: StandardRouter,
-    printingEnabled
+    const {loadMaps} = require('../MapStore2/web/client/actions/maps');
+
+    const StandardApp = require('../MapStore2/web/client/components/app/StandardApp');
+
+    const {pages, pluginsDef, initialState, storeOpts} = require('./appConfig');
+    LocaleUtils.setSupportedLocales({
+         "it": {
+             code: "it-IT",
+             description: "Italiano"
+         },
+         "de": {
+           code: "de-DE",
+           description: "Deutsch"
+         }
+    });
+    const StandardRouter = connect((state) => ({
+        locale: state.locale || {},
+        pages
+    }))(require('../MapStore2/web/client/components/app/StandardRouter'));
+
+    const appStore = require('../MapStore2/web/client/stores/StandardStore').bind(null, initialState, {
+        maps: require('../MapStore2/web/client/reducers/maps'),
+        security: require('../MapStore2/web/client/reducers/security')
+    }, {registerSearchServiceEpic});
+
+    const initialActions = [
+        () => loadMaps(ConfigUtils.getDefaults().geoStoreUrl, ConfigUtils.getDefaults().initialMapFilter || "*")
+    ];
+
+    const appConfig = {
+        storeOpts,
+        appStore,
+        pluginsDef,
+        initialActions,
+        appComponent: StandardRouter,
+        printingEnabled: true
+    };
+
+    ReactDOM.render(
+        <StandardApp {...appConfig}/>,
+        document.getElementById('container')
+    );
 };
 
-ReactDOM.render(
-    <StandardApp {...appConfig}/>,
-    document.getElementById('container')
-);
+if (!global.Intl ) {
+    // Ensure Intl is loaded, then call the given callback
+    LocaleUtils.ensureIntl(startApp);
+}else {
+    startApp();
+}
