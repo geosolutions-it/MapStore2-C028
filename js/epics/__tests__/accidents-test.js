cc506dfb9cdc059a1a329ec55a1f715681e57fb6
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2018-07-24 18:37:48 +0200

    updated test due to rencent fixes on date picker

--- js/epics/__tests__/accidents-test.js
+++ js/epics/__tests__/accidents-test.js
@@ -60,9 +60,9 @@ describe('accidents epic', () => {
             expect(actions[0].type).toBe(CHANGE_LAYER_PARAMS);
             expect(actions[1].type).toBe(CHANGE_LAYER_PARAMS);
             const vpAction = actions[0].layer === 1 ? actions[0] : actions[1];
-            expect(vpAction.params.VIEWPARAMS).toBe("dow_p:1;tpinc_p:1;fromdate_p:1492-10-11;todate_p:1789-07-13");
+            expect(vpAction.params.VIEWPARAMS).toBe("dow_p:1;tpinc_p:1;fromdate_p:1492-10-12;todate_p:1789-07-14");
             const cqlAction = actions[0].layer === 2 ? actions[0] : actions[1];
-            expect(cqlAction.params.CQL_FILTER).toBe("( DTINCID <= '1789-07-13' AND DTINCID >= '1492-10-11' ) AND (DOW='1') AND (TPINCID='1')");
+            expect(cqlAction.params.CQL_FILTER).toBe("( DTINCID <= '1789-07-14' AND DTINCID >= '1492-10-12' ) AND (DOW='1') AND (TPINCID='1')");
             done();
         }, STATE);
     });
